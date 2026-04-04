50e4c7ff9ae6fef07ba7b678bc320e6b4907bffb
thyrsonresquin <thyrson@outlook.com>
2026-04-03 21:10:20 -0400

    problema1038

---
+++ problema1038.py
@@ -0,0 +1,17 @@
+x, y = map(int, input().split())
+
+if x == 1:
+    z = 4.0
+    print('Total: R$ {:.2f}'.format(y * z))
+elif x == 2:
+    z = 4.5
+    print('Total: R$ {:.2f}'.format(y * z))
+elif x == 3:
+    z = 5.0
+    print('Total: R$ {:.2f}'.format(y * z))
+elif x == 4:
+    z = 2.0
+    print('Total: R$ {:.2f}'.format(y * z))
+elif x == 5:
+    z = 1.5
+    print('Total: R$ {:.2f}'.format(y * z))
